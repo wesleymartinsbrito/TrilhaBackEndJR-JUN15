0f009e02670e6c683b176b05a42c74c9f8c1a082
Wesley Martins <wesleymartinsjnr@gmail.com>
2024-07-01 17:23:04 -0300

    first commit

---
+++ src/main/java/com/wesleymartins/codigocerto/CodigoCertoApplication.java
@@ -0,0 +1,13 @@
+package com.wesleymartins.codigocerto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodigoCertoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodigoCertoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wesleymartins/codigocerto/CodigoCertoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wesleymartins.codigocerto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodigoCertoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
