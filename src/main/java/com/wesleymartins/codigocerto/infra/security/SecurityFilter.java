4733a4821be29b9e09c9e50375e1988d84d66675
Wesley Martins <wesleymartinsjnr@gmail.com>
2024-07-01 19:37:03 -0300

    update vercel

--- src/main/java/com/wesleymartins/codigocerto/infra/security/SecurityFilter.java
+++ src/main/java/com/wesleymartins/codigocerto/infra/security/SecurityFilter.java
@@ -28,7 +28,7 @@ public class SecurityFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         var token = this.recoverToken(request);
         if(token != null){
-            var subject = tokenService.validateToken(token);
+            var subject = tokenService.validateToken((String) token);
             UserDetails user = userRepository.findByEmail(subject);
 
             var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
