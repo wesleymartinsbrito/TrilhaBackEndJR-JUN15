10039980ddede8a1add4d4fb182eb5afa0b976a4
Wesley Martins <wesleymartinsjnr@gmail.com>
2024-07-01 18:59:45 -0300

    Generate JWT token created

--- src/main/java/com/wesleymartins/codigocerto/infra/security/SecurityConfiguration.java
+++ src/main/java/com/wesleymartins/codigocerto/infra/security/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.wesleymartins.codigocerto.infra.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -20,6 +21,8 @@ public class SecurityConfiguration {
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
                         .anyRequest().authenticated())
                 .build();
     }
