0fab5107199b7a3d16c8576df7614a89859a5afb
Wesley Martins <wesleymartinsjnr@gmail.com>
2024-07-30 16:35:29 -0300

    updates 3

--- src/main/java/com/wesleymartins/codigocerto/repositories/TarefaRepository.java
+++ src/main/java/com/wesleymartins/codigocerto/repositories/TarefaRepository.java
@@ -2,6 +2,8 @@ package com.wesleymartins.codigocerto.repositories;
 
 import com.wesleymartins.codigocerto.entities.Tarefa;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TarefaRepository extends JpaRepository<Tarefa, Long> {
 }
--- src/main/java/com/wesleymartins/codigocerto/repositories/UserRepository.java
+++ src/main/java/com/wesleymartins/codigocerto/repositories/UserRepository.java
@@ -3,7 +3,9 @@ package com.wesleymartins.codigocerto.repositories;
 import com.wesleymartins.codigocerto.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     UserDetails findByEmail(String email);
 }
